f266ad0245ec3da841f41792901c0f9913c1be54
KillianMHR <cyvwrsbrsm@privaterelay.appleid.com>
2026-09-19 18:18:32 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='project4808-v2-2-animated-20260919';
+const CACHE='project4808-v2-3-checkpoint-feedback-20260919';
 self.addEventListener('install',e=>e.waitUntil(self.skipWaiting()));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
 self.addEventListener('fetch',e=>{
