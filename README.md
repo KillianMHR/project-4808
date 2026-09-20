452bdcd9be02624d74fb1d360b2ac9c5141ff866
KillianMHR <cyvwrsbrsm@privaterelay.appleid.com>
2026-09-20 03:04:59 +0200

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # HIKE+ V1.13
-Version Pré-bêta. Correction renforcée de l’accès pré-bêta sur iPhone/Safari.
+Version Pré-bêta avec ouverture publique programmée au samedi 3 octobre 2026 à 10h00 (heure de France métropolitaine).
+
+Mise à jour : ajout des informations de stationnement pour la randonnée « Sentier du contrebandier » à Jobourg. Le départ se fait depuis le parking du Nez de Jobourg, 7 Rue des Falaises.
--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='hikeplus-v1-13-prebeta';
+const CACHE='hikeplus-v1-13-parking';
 const CORE=['./','./index.html','./manifest.json','./hike-logo.png'];
 self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(CORE)).then(()=>self.skipWaiting()));});
 self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()));});
